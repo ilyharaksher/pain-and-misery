3b9b73e145bddde32cf354ee60f4aa55fa8c6879
ilyharaksher <122717733+ilyharaksher@users.noreply.github.com>
2023-04-26 12:52:13 +0300

    Program added

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int s = 0;
+Array.Sort(arr);
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] % 2 == 0)
+    {
+        s = arr[i];
+        break;
+    }
+}
+if (!Convert.ToBoolean(s))
+{
+    Console.WriteLine("Четных чисел нету");
+}
+else{
+    Console.WriteLine(s);
+}
